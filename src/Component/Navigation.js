db4a741090614e9935a58568dc0eb2bc7d9aca00
tanuj <tanuj@metaspacechain.com>
2023-06-15 14:28:31 +0530

    changes for add delete and edit venod

--- src/Component/Navigation.js
+++ src/Component/Navigation.js
@@ -201,7 +201,6 @@ function Navigation(props) {
         headers: { _token: authData?.data?.token },
       })
       .then((response) => {
-        console.log(response.data.data);
         setNotificationData(
           response.data.data.sort((a, b) => b.createdAt - a.createdAt)
         );
