ceecd3b177fc96a37532d126feb01673a615552e
tanuj <tanuj@metaspacechain.com>
2023-12-30 14:40:28 +0530

    crated build

--- src/Pages/admin/ChatSupport.jsx
+++ src/Pages/admin/ChatSupport.jsx
@@ -322,7 +322,7 @@ const ChatSupport = () => {
         activeChannel.off("messageAdded", handleNewMessage);
       }
     };
-  }, [activeChannel,messages]);
+  }, [activeChannel]);
 
   useEffect(() => {
     console.log("Client changed...");
@@ -358,7 +358,7 @@ const ChatSupport = () => {
     if (authData) {
       initializeChatClient();
     }
-  }, []);
+  }, [messages]);
 
   console.log("messages", allChannel);
 
